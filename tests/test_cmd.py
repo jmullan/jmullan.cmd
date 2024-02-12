19f1f71edac4bc8bc72f9520ccc4bc4b667f964a
Jesse Mullan <jmullan@gmail.com>
2024-02-12 11:00:55 -0800

    Initial commit

---
+++ tests/test_cmd.py
@@ -0,0 +1,7 @@
+from jmullan_cmd import cmd
+
+
+def test_main():
+    # this is just a stub that only tests that we can load the module and use the class
+    main = cmd.Main()
+    assert main is not None
